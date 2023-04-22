c7501ed10ec9c0634fb65ac35719bc127af66416
veet <veetmoradiya3628@gmail.com>
2023-04-22 23:43:06 +0530

    Quiz, Category and Question model code added

---
+++ examserver/src/main/java/com/exam/examserver/entity/exam/Quiz.java
@@ -0,0 +1,86 @@
+package com.exam.examserver.entity.exam;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+public class Quiz {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long qId;
+
+    private String title;
+    private String description;
+    private String maxMarks;
+
+    private String numberOfQuestions;
+    private boolean active = false;
+    @ManyToOne(fetch = FetchType.EAGER)
+    private Category category;
+
+    @OneToMany(mappedBy = "quiz", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JsonIgnore
+    private Set<Question> questions = new HashSet<>();
+
+    public Quiz(){}
+
+    public Long getQid() {
+        return qId;
+    }
+
+    public void setQid(Long qid) {
+        this.qId = qid;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getMaxMarks() {
+        return maxMarks;
+    }
+
+    public void setMaxMarks(String maxMarks) {
+        this.maxMarks = maxMarks;
+    }
+
+    public String getNumberOfQuestions() {
+        return numberOfQuestions;
+    }
+
+    public void setNumberOfQuestions(String numberOfQuestions) {
+        this.numberOfQuestions = numberOfQuestions;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+}
