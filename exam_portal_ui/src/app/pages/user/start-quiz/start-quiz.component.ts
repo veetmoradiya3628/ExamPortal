d06b9e2e1df52f0561d16ec276f4aa4ae5ac272f
veet <veetmoradiya3628@gmail.com>
2023-05-01 19:07:21 +0530

    Quiz evaluation on client implemented

--- exam_portal_ui/src/app/pages/user/start-quiz/start-quiz.component.ts
+++ exam_portal_ui/src/app/pages/user/start-quiz/start-quiz.component.ts
@@ -13,24 +13,36 @@ export class StartQuizComponent implements OnInit {
   qid = 0;
   questions: any;
 
-  constructor(private _route: ActivatedRoute, private _locationStrategy: LocationStrategy, private _questionService: QuestionService){}
+  marksGot = 0;
+  correctAnswers = 0;
+  attempted = 0;
+  isSubmit = false;
+
+
+  constructor(private _route: ActivatedRoute, private _locationStrategy: LocationStrategy, private _questionService: QuestionService) { }
   ngOnInit(): void {
     this.qid = this._route.snapshot.params['qid'];
     this.preventBackButton();
     this.loadQuestions();
   }
 
-  preventBackButton(){
-    history.pushState(null, null, location.href);
+  preventBackButton() {
+    history.pushState(null, 'null', location.href);
     this._locationStrategy.onPopState(() => {
-      history.pushState(null, null, location.href);
+      history.pushState(null, 'null', location.href);
     })
   }
 
-  loadQuestions(){
+  loadQuestions() {
     this._questionService.getQuestionOfQuizForTest(this.qid).subscribe(
       (data: any) => {
         this.questions = data;
+
+        this.questions.forEach((question: any) => {
+          question['givenAnswer'] = '';
+        });
+        console.log(`Question data on quiz is : `);
+        console.log(this.questions);
       },
       (error: any) => {
         Swal.fire('Error', 'Error in loading Question of Quiz', 'error');
@@ -38,4 +50,31 @@ export class StartQuizComponent implements OnInit {
     )
   }
 
+
+  submitQuiz() {
+    Swal.fire({
+      title: 'Do you want to end the quiz?',
+      showCancelButton: true,
+      confirmButtonText: 'Submit',
+      icon: 'info'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        // calculation
+        this.isSubmit = true;
+        this.questions.forEach((ques: any) => {
+          if (ques.givenAnswer == ques.answer) {
+            this.correctAnswers++;
+            let marksSingle = this.questions[0].quiz.maxMarks / this.questions.length;
+            this.marksGot += marksSingle;            
+          }
+          if(ques.givenAnswer.trim() != ''){
+            this.attempted++;
+          }
+        })
+        console.log('Correct answers : '+this.correctAnswers);
+        console.log('Marks got : '+this.marksGot);
+        console.log('Attampted : '+this.attempted);
+      }
+    })
+  }
 }
