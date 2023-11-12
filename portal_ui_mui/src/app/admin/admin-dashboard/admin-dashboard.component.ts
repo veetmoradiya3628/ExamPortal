c8950e1d94fab8c2a6a06d62969423da7e62663e
veetmoradiya <veetmoradiya3628@gmail.com>
2023-11-12 17:30:36 +0530

    UI dashboard for org-admin teacher role added

--- portal_ui_mui/src/app/admin/admin-dashboard/admin-dashboard.component.ts
+++ portal_ui_mui/src/app/admin/admin-dashboard/admin-dashboard.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Chart, registerables } from 'chart.js';
+Chart.register(...registerables);
 
 @Component({
   selector: 'app-admin-dashboard',
   templateUrl: './admin-dashboard.component.html',
   styleUrls: ['./admin-dashboard.component.scss']
 })
-export class AdminDashboardComponent {
+export class AdminDashboardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.renderStatsChart();
+  }
+
+  renderStatsChart() {
+    const orgChart = new Chart("orgchart", {
+      type: 'bar',
+      data: {
+        labels: ['Org1', 'Org2', 'Org3', 'Org4234536755543425367565432', 'Purple', 'Orange', 'Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+          label: '# of Teachers',
+          data: [12, 19, 3, 5, 2, 3, 12, 19, 3, 5, 2, 3],
+          borderWidth: 1
+        },
+        {
+          label: '# of Students',
+          data: [8, 10, 5, 2, 9, 10, 12, 11, 0, 12, 1, 9],
+          borderWidth: 1
+        }]
+      },
+      options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
+  }
 
 }
--- portal_ui_mui/src/app/org-admin/org-admin-dashboard/org-admin-dashboard.component.ts
+++ portal_ui_mui/src/app/org-admin/org-admin-dashboard/org-admin-dashboard.component.ts
@@ -1,10 +1,87 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Chart, registerables } from 'chart.js';
+Chart.register(...registerables);
 
 @Component({
   selector: 'app-org-admin-dashboard',
   templateUrl: './org-admin-dashboard.component.html',
   styleUrls: ['./org-admin-dashboard.component.scss']
 })
-export class OrgAdminDashboardComponent {
+export class OrgAdminDashboardComponent implements OnInit {
 
+  constructor() { }
+
+  ngOnInit(): void {
+    this.renderGraphs();
+    this.renderSecondGraph();
+  }
+
+  renderGraphs() {
+    const orgChart = new Chart("orgchart", {
+      type: 'bar',
+      data: {
+        labels: ['class1', 'class2', 'class3', 'class4', 'class5', 'class6'],
+        datasets: [{
+          label: '# of Teachers',
+          data: [12, 19, 3, 5, 2, 3],
+          borderWidth: 1
+        },
+        {
+          label: '# of Students',
+          data: [8, 10, 5, 2, 9, 10],
+          borderWidth: 1
+        },
+        {
+          label: '# of Quizzes',
+          data: [2, 3, 8, 2, 4, 10],
+          borderWidth: 1
+        }]
+      },
+      options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
+  }
+
+  renderSecondGraph() {
+    const orgChart = new Chart("attemptChart", {
+      type: 'bar',
+      data: {
+        labels: ['quiz1', 'quiz2', 'quiz3', 'quiz4', 'quiz5', 'quiz6'],
+        datasets: [{
+          label: '# of Attempts',
+          data: [12, 19, 3, 5, 2, 3],
+          borderWidth: 1,
+          backgroundColor: '#4bc0c0'
+        },
+        {
+          label: '# of Not Attempts',
+          data: [8, 10, 5, 2, 9, 10],
+          borderWidth: 1,
+          backgroundColor: '#ff6384'
+        }]
+      },
+      options: {
+        indexAxis: 'y',
+        responsive: true,
+        maintainAspectRatio: false,
+        scales: {
+          y: {
+            beginAtZero: true,
+          }
+        },
+        plugins: {
+          legend: {
+            position: 'top',
+          },
+        }
+      }
+    });
+  }
 }
